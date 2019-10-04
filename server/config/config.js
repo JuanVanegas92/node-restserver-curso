a0c6e7374bb2d5205b9daf7526e198cd1fbf4f03
Juan <ne_34vanegas@hotmail.com>
2019-10-04 14:19:24 -0500

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 //  PUERTO
 // ===============================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ===============================================
+//  ENTORNO
+// ===============================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===============================================
+//  BASE DE DATOS
+// ===============================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://juan:patito.1234@cafe-w61g5.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,33 +12,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-});
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err, res) => {
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
