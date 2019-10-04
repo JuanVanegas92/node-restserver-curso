7598b28b7000b1bfa6bafe68859378916e8eeecc
Juan <ne_34vanegas@hotmail.com>
2019-10-04 17:59:34 -0500

    Variable de entorno MONGODB

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://juan:patito.1234@cafe-w61g5.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
