5e8e04cbd843fef6c3221bc42aac8418b7185af7
Juan <ne_34vanegas@hotmail.com>
2019-10-14 22:03:05 -0500

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,22 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ===============================================
-//  BASE DE DATOS
+//  Vencimiento del Token
+// ===============================================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ===============================================
+//  SEED de autenticacion
+// ===============================================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+
+// ===============================================
+//  BASE DE DATOS  'este-es-el-seed-produccion'
 // ===============================================
 let urlDB;
 
